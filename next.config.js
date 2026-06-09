cea796bb428f76272860506725203dfacf30ec5a
shoojjjj <shoojjjj@gmail.com>
2026-06-09 19:41:24 +0900

    downgrade yahoo-finance2 to 2.9

--- next.config.js
+++ next.config.js
@@ -1,6 +1,2 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  serverExternalPackages: ['yahoo-finance2'],
-}
+const nextConfig = { reactStrictMode: true }
 module.exports = nextConfig
