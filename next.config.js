bbadd49355e807fb60976629fb31a98e0bd84e09
shoojjjj <shoojjjj@gmail.com>
2026-06-09 19:39:12 +0900

    serverExternalPackages yahoo-finance2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-      config.externals.push('yahoo-finance2');
-    }
-    return config;
-  },
+  serverExternalPackages: ['yahoo-finance2'],
 }
 module.exports = nextConfig
